2dc451988e52ac6b622caed582f50f0dbb772a28
nawaf-almalki <Almalki.K.Nawaf@gmail.com>
2024-05-14 23:15:18 +0300

    brought db back local

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })   .then(() => console.log('MongoDB connected'))   .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))   
+    .catch(err => console.log(err));
 module.exports = mongoose;
